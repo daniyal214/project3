5d230336484aac27b878908183626fb88da385de
Muhammad Daniyal <daniyal.hashmi2010@yahoo,com>
2020-06-11 04:16:55 +0500

    'createapp.js'

--- src/App.js
+++ src/App.js
@@ -12,27 +12,7 @@ function App({name, age}) {
           <br/>
           <Hello firstname={name}> </Hello>
           </div>   
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br/>
-          Updated by Daniyal
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}}
+
+}
 
 export default App;
